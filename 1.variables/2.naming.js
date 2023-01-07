d41b759970bbca0dae20ff11ea59c10eed2eae5f
suha <gusuhawoo@gmail.com>
2023-01-07 19:10:01 +0900

    Add : variable rules

---
+++ 1.variables/2.naming.js
@@ -0,0 +1,9 @@
+// **Variable Rules**
+// Latin alphabet(0-9,a-z,A-Z), _
+// Case sensitive(camelCase)
+// Do not use reserved words
+// ex) 1.let if;
+//     2.let for;
+// Don't start with a number
+// Do not use special characters(_,$ are two exceptions.)
+// Emoticons are prohibited
