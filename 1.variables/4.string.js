77c95d587fa6c34688fa42a9fb325f9048bd1e50
suha <gusuhawoo@gmail.com>
2023-01-10 15:03:05 +0900

    Add : variable(string)

---
+++ 1.variables/4.string.js
@@ -0,0 +1,16 @@
+// String Type
+let string = 'hello JS!';
+string = `hello`;
+console.log(string);
+string = '"JS"';
+console.log(string);
+string = 'hello!\nJS\t\tthis\\is\u09ac';
+console.log(string);
+
+//Template Literal
+let id = 'suha';
+let greetings = 'hello!,' + id + '👏🏻\nhave a nice day!';
+console.log(greetings);
+
+greetings = `'hello!,${id}👏🏻\nhave a nice time!`;
+console.log(greetings);
