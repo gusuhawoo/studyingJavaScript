44d305e94bb9bbdde255ef3ae4155c4e8e484dc5
suha <gusuhawoo@gmail.com>
2023-01-10 15:11:55 +0900

    Add : variable(boolean)

--- 1.variables/4.string.js
+++ 1.variables/4.string.js
@@ -7,7 +7,7 @@ console.log(string);
 string = 'hello!\nJS\t\tthis\\is\u09ac';
 console.log(string);
 
-//Template Literal
+//Template Literal '', "", ``
 let id = 'suha';
 let greetings = 'hello!,' + id + '👏🏻\nhave a nice day!';
 console.log(greetings);
---
+++ 1.variables/5.boolean.js
@@ -0,0 +1,26 @@
+// Boolean Type
+let true1 = true;
+let false1 = false;
+console.log(true1);
+console.log(false1);
+
+let isFree = true;
+let isActivated = false;
+let isEntrolled = true;
+console.log(isActivated);
+
+console.clear();
+// Falshy
+console.log(!!0);
+console.log(!!-0);
+console.log(!!'');
+console.log(!!null);
+console.log(!!undefined);
+console.log(!!NaN);
+
+// Truthy
+console.log(!!1);
+console.log(!!-1);
+console.log(!!'text');
+console.log(!!{});
+console.log(!!Infinity);
