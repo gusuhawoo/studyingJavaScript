ba9a4cf00e0b2c8d7cff55078709d481528ac7e4
suha <gusuhawoo@gmail.com>
2023-01-11 14:09:51 +0900

    Add : operator(priority)

---
+++ 2.operators/7.priority.js
@@ -0,0 +1,10 @@
+let a = 2;
+let b = 3;
+let result = a + b * 4; // a + (b * 4)
+console.log(result); // 14
+result = a++ + b * 4; // 2 + (b * 4)
+console.log(result); // 14
+
+/*
+  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Operator_Precedence
+*/
