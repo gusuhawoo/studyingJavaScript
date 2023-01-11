7bbefe8cc57befded53da80da340cd278946d529
suha <gusuhawoo@gmail.com>
2023-01-11 13:07:46 +0900

    Add : operator(arithmetic)

---
+++ 2.operators/2.arithmetic.js
@@ -0,0 +1,21 @@
+// Arithmetic operators
+// + Plus
+// - Minus
+// * Multiplication
+// / Division
+// % Remainder
+// ** Power
+
+console.log(5 + 2); // 7
+console.log(5 - 2); // 3
+console.log(5 * 2); // 10
+console.log(5 / 2); // 2.5
+console.log(5 % 2); // 1
+console.log(5 ** 2); // 25 Power(es7)
+console.log(Math.pow(5, 2)); //25 Power
+
+// + Plus
+let text = 'two ' + 'letters';
+console.log(text); // 'two letters'
+text = '1' + 1; // number + string = string
+console.log(text); // '11'
