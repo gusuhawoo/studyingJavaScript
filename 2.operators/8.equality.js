d4a5558c57258c47dc084eb50d2728183b166b8a
suha <gusuhawoo@gmail.com>
2023-01-11 14:18:56 +0900

    Add : operator(equality)

---
+++ 2.operators/8.equality.js
@@ -0,0 +1,32 @@
+// Equality Operators
+// ==
+// !=
+// ===
+// !==
+console.log(2 == 2); // true
+console.log(2 != 2); // false
+console.log(2 != 3); // true
+console.log(2 == 2); // true
+console.log(2 == '2'); // true
+console.log(2 === '2'); // false
+console.log(true == 1); // true
+console.log(true === 1); // false
+console.log(false == 0); // true
+console.log(false === 0); // false
+
+console.clear();
+
+const obj1 = {
+  name: 'js',
+};
+const obj2 = {
+  name: 'js',
+};
+console.log(obj1 == obj2); // false
+console.log(obj1 === obj2); // false
+console.log(obj1.name == obj2.name); // true
+console.log(obj1.name === obj2.name); // true
+
+let obj3 = obj2;
+console.log(obj3 == obj2); // true
+console.log(obj3 === obj2); // true
