d43593e12613ceca9dba546722d0896342a59c9d
suha <gusuhawoo@gmail.com>
2023-01-11 15:17:32 +0900

    Add : control statement(if)

---
+++ 3.controlStatement/1.if.js
@@ -0,0 +1,12 @@
+// Conditional Statement
+// if() {}
+// if() {} else {}
+// if() {} else if() {} else {}
+let fruit = 'apple';
+if (fruit === 'apple') {
+  console.log('🍎');
+} else if (fruit === 'orange') {
+  console.log('🍊');
+} else {
+  console.log('!!');
+}
