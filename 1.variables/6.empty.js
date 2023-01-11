9c1f4384b0c28871cecab3fae34ad9cc1b27256c
suha <gusuhawoo@gmail.com>
2023-01-11 11:05:55 +0900

    Add : variable(empty:null, undefined)

---
+++ 1.variables/6.empty.js
@@ -0,0 +1,12 @@
+// null, undefined
+let variable;
+console.log(variable);
+
+variable = null;
+console.log(variable);
+
+let activeItem; // unallocated state
+activeItem = null; // no active item
+
+console.log(typeof null); // object
+console.log(typeof activeItem);
