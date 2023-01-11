ec9fa88b52bce1f4bc9c2a01ecac8ea87f667393
suha <gusuhawoo@gmail.com>
2023-01-11 13:35:58 +0900

    Add : operator(assignment)

---
+++ 2.operators/4.assignment.js
@@ -0,0 +1,22 @@
+// Assignment Operators
+let a = 1;
+a = a + 2;
+console.log(a);
+
+a += 2; // a = a + 2;
+console.log(a); // 5
+
+a -= 2;
+console.log(a); // 3
+
+a *= 2;
+console.log(a); // 6
+
+a /= 2;
+console.log(a); //3
+
+a %= 2;
+console.log(a); //1
+
+a **= 2;
+console.log(a); //1
