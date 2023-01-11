ee255be3435ed1df81f200cffadfaaee0598fb86
suha <gusuhawoo@gmail.com>
2023-01-11 15:26:06 +0900

    Add : control statement(ternary)

---
+++ 3.controlStatement/2.ternary.js
@@ -0,0 +1,13 @@
+// Ternary Operator
+// ? :
+let fruit = 'orange';
+if (fruit === 'apple') {
+  console.log('🍎');
+} else {
+  console.log('🍊');
+}
+
+fruit === 'apple' ? console.log('🍎') : console.log('🍊');
+
+let emoji = fruit === 'apple' ? '🍎' : '🍊';
+console.log(emoji);
