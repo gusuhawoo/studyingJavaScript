e18edafcd4ab8d82f28d02284ab571dc38935bd6
suha <gusuhawoo@gmail.com>
2023-01-11 14:00:30 +0900

    operator(relational)

---
+++ 2.operators/6.relational.js
@@ -0,0 +1,13 @@
+// Relational Operators
+// >
+// <
+// >=
+// <=
+console.log(2 > 3); // false
+console.log(2 < 3); // true
+console.log(3 < 2); // false
+console.log(3 > 2); // true
+console.log(3 <= 2); // false
+console.log(3 <= 3); // true
+console.log(3 >= 3); // true
+console.log(3 >= 2); // true
