5fb853959bcc4472ad1102368df89ee9bb1c3c34
suha <gusuhawoo@gmail.com>
2023-01-11 13:42:33 +0900

    Add : operator(increment)

---
+++ 2.operators/5.increment.js
@@ -0,0 +1,16 @@
+// Increment & Decrement Operators
+let a = 0;
+console.log(a); // 0
+a++; // a = a + 1;
+console.log(a); // 1
+a--; // a = a -1;
+console.log(a); // 0
+
+console.clear();
+// a++
+// ++a
+a = 0;
+// let b = a++;
+// console.log(b); // 0
+let b = ++a;
+console.log(b); // 1
