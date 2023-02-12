04cb1c4799d0a146f9b78e35ce9deeff23f805c6
suha <gusuhawoo@gmail.com>
2023-02-12 15:37:11 +0900

    Add : class

---
+++ 6.class/1.basic.js
@@ -0,0 +1,31 @@
+// constructor function
+// function Fruit(name, emoji) {
+//   this.name = name;
+//   this.emoji = emoji;
+//   this.display = () => {
+//     console.log(`${this.name}: ${this.emoji}`);
+//   };
+//   return this;
+// }
+
+// const apple = new Fruit('apple', '🍎');
+// const orange = new Fruit('orange', '🍊');
+
+// class
+class Fruit {
+  constructor(name, emoji) {
+    this.name = name;
+    this.emoji = emoji;
+  }
+  display = () => {
+    console.log(`${this.name}: ${this.emoji}`);
+  };
+}
+
+const apple = new Fruit('apple', '🍎');
+const orange = new Fruit('orange', '🍊');
+
+console.log(apple);
+console.log(orange);
+console.log(apple.name);
+console.log(orange.display);
