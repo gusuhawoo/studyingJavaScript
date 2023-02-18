fcef34059454b219618f15f7da1a3bf128d384db
suha <gusuhawoo@gmail.com>
2023-02-18 17:56:44 +0900

    Add : module(comments)

---
+++ 12.module/1.comments.js
@@ -0,0 +1,26 @@
+// Comments
+// One line
+// TODO(suha): To Do list
+
+/**
+ * WHY && HOW
+ */
+
+// functionAPI used JSDoc
+/**
+ *
+ * @param {*} a number1
+ * @param {*} b number2
+ * @returns a + b
+ */
+function add(a, b) {
+  return a + b;
+}
+
+[].map
+map<U>(callbackfn: (value: T, index: number, array: T[]) => U, thisArg?: any): U[];
+    /**
+     * Returns the elements of an array that meet the condition specified in a callback function.
+     * @param predicate A function that accepts up to three arguments. The filter method calls the predicate function one time for each element in the array.
+     * @param thisArg An object to which the this keyword can refer in the predicate function. If thisArg is omitted, undefined is used as the this value.
+     */
