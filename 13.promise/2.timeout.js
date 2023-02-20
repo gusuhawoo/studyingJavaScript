5bd541585c59ce78943ba497592d689ed773864a
suha <gusuhawoo@gmail.com>
2023-02-20 14:17:26 +0900

    Add : promise(setTimeout, try&catch)

--- 13.promise/2.timeout.js
+++ 13.promise/2.timeout.js
@@ -24,3 +24,29 @@ execute();
 3
 2
 */
+
+// Callback function is called after a set number of seconds.
+// Displays an error if seconds are less than 0.
+function runInDelay(callback, seconds) {
+  if (!callback) {
+    throw new Error('You must pass a callback function.');
+  }
+  if (!seconds || seconds < 0) {
+    throw new Error('seconds must be greater than 0.');
+  }
+  setTimeout(callback, seconds * 1000);
+}
+
+// runInDelay(() => {
+//   console.log('Timer Completed');
+// }, 2);
+
+// runInDelay(() => {
+//   console.log('Timer Completed'); // Error
+// }, -1);
+
+try {
+  runInDelay(() => {
+    console.log('Timer Completed');
+  }, -2);
+} catch (error) {}
