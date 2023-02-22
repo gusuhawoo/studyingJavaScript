5ee1dd627cc050b4302a943851657206a0e5b8a2
suha <gusuhawoo@gmail.com>
2023-02-22 17:06:34 +0900

    Add : promise(JSON API)

---
+++ 13.promise/8.fetch/8.fetch.js
@@ -0,0 +1,7 @@
+// fetch()
+/*
+https://developer.mozilla.org/en-US/docs/Web/API/fetch
+The global fetch() method starts the process of fetching a resource from the network, 
+returning a promise which is fulfilled once the response is available.
+The promise resolves to the Response object representing the response to your request.
+*/
